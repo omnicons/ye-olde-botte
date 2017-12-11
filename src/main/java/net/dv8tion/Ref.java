d3bc66203468d84ee8d127cabecec630b93e01d7
RinSnowMew <hollownote@gmail.com>
2017-12-11 03:11:28 -0500

    BotTokenRedacted

--- src/main/java/net/dv8tion/Ref.java
+++ src/main/java/net/dv8tion/Ref.java
@@ -2,7 +2,7 @@ package main.java.net.dv8tion;
 
 public class Ref 
 {
-	public static final String Token = "Mzg4NTM3NDM5Mjk3NDcwNDc2.DQu48g.O7gD9qKuiedIipcjB1IkKZW9vn0";
-	public static final String CliID = "388537439297470476";
+	public static final String Token = "TOKEN";
+	public static final String CliID = "CliID";
 	public static final char trigger = '!';
 }
